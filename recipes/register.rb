2284a208c9d48ab77f173f00e4ac0b46c91f8c54
opanmustopah <opan.mustopah@go-jek.com>
2018-05-28 13:43:40 +0700

    Include recipe register to default recipe

--- recipes/register.rb
+++ recipes/register.rb
@@ -2,12 +2,12 @@
 # Recipe:: register
 #
 
-if node[cookbook_name]['registered_services'].length > 0
-  consul_register_service "Register Zookeeper as service in Consul agent" do
-    configs     node[cookbook_name]['registered_services']
-    config_dir  node['consul']['config_dir']
-    exec_file   "#{node['consul']['prefix_home']}/bin/consul"
-    user        node['consul']['user']
-    group       node['consul']['group']
-  end
+return if node[cookbook_name]['registered_services'].length > 0
+
+consul_register_service "Register Zookeeper as service in Consul agent" do
+  configs     node[cookbook_name]['registered_services']
+  config_dir  node['consul']['config_dir']
+  exec_file   "#{node['consul']['prefix_home']}/bin/consul"
+  user        node['consul']['user']
+  group       node['consul']['group']
 end
