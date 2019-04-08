64587cd3cc70d53ca4ae25e0a0dd0cd70077c6f1
Deny Prasetyo <jasoet87@gmail.com>
2019-04-08 13:52:37 +0700

    [Jasoet] Remove unused test line

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -12,8 +12,6 @@ describe 'zookeeper::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      mock_result = {zookeeper_hosts: ['zookeeper-01-ubuntu-1804.vagrantup.com', 'zookeeper-02-ubuntu-1804.vagrantup.com']}
-      allow_instance_of(Yggdrasil).to receive(:fetch_configs).and_return(mock_result)
       runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
