fd3bc3aff509b9b96ca14c8c3f6b504c93da047d
Beni Budiharto <gnu.iben@gmail.com>
2019-08-29 17:26:56 +0700

    add consul healthcheck

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -70,3 +70,9 @@ describe systemd_service('zookeeper') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe systemd_service('consul') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
